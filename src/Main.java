501776fc2fe3adfabb5cc5015589e2a5ffda2913
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-11-11 23:24:31 -0300

    Add comment

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Based on Dev.java tutorials
+ *
+ * @see <a href="https://dev.java/learn/annotations">Annotations - Dev.java</a>
+ */
 void main() {
     Blog.of("pedroreis.dev").build();
 }
